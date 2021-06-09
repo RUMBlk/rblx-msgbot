f4e91d04412e06f57fc4a505969346c6225425b5
RUMBlk <60323621+RUMBlk@users.noreply.github.com>
2021-06-09 14:20:54 +0300

    Add files via upload

---
+++ msglist.lua
@@ -0,0 +1,13 @@
+msg = 
+{
+    "Go to #funkyquotes to add your quotes and vote for others",
+    "funky!",
+    "Join \"funky!\" DS too! Search -> #funkyfumo in twitter for link!",
+    "Stay Funky!",
+    "The funky tree rises once again!",
+    "We do a bit of funkying",
+    "What a beautiful F U N K Y!",
+    "All you have to do is being funky!",
+    "reject humanity, become funky",
+    "What a funky!"
+}
