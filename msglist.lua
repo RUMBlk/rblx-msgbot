449ead494c4a24b730b01331e156872cc962b160
RUMBlk <60323621+RUMBlk@users.noreply.github.com>
2021-06-09 14:57:13 +0300

    Add files via upload

--- msglist.lua
+++ msglist.lua
@@ -1,13 +1,17 @@
+msgversion = "150009062021"
 msg = 
 {
-    "Go to #funkyquotes to add your quotes and vote for others",
+    "Go to #funkyquotes to add your quotes and vote for others. Version of quotes: " + msgversion,
     "funky!",
     "Join \"funky!\" DS too! Search -> #funkyfumo in twitter for link!",
     "Stay Funky!",
     "The funky tree rises once again!",
     "We do a bit of funkying",
-    "What a beautiful F U N K Y!",
+    "What a beautiful day to be FUNKY!",
     "All you have to do is being funky!",
     "reject humanity, become funky",
     "What a funky!"
+	"Funky is Eternal",
+	"This messages were sended by a bot, my executor could be now afk",
+	"Funky is Eternal, not funky is not"
 }
