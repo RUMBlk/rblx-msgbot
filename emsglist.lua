2afc092367ee5f99b5abb265d0ffcf9787ff1b3b
RUMBlk <60323621+RUMBlk@users.noreply.github.com>
2021-06-10 12:26:35 +0300

    Add files via upload

---
+++ emsglist.lua
@@ -0,0 +1,37 @@
+msgcheck = msgversion
+msgversion = "1.3E - Experimental update"
+desc = 
+{
+    "Go to #funkyquotes in our DS server to add your quotes and vote for others. Script was made by RUMBlk",
+    "Join \"funky!\" DS today! Search -> #funkyfumo in twitter for link!"
+}
+msg = 
+{
+    "funky!",
+    "Stay Funky!",
+    "The funky tree rises once again!",
+    "We do a bit of funkying",
+    "What a beautiful day to be FUNKY!",
+    "All you have to do is being funky!",
+    "Reject humanity, become funky",
+    "What a funky!",
+    "This messages was sended by a bot, my executor could be now afk",
+    "Funky is Eternal, not funky is not",
+    "Enlist to the \"Funky! Patrol\" today and serve your country",
+    "Funky together strong",
+    "100% pure, concentrated funky",
+    "You save the funky, you save the world",
+    "FumoCam is funky, don't forget to check BecomeFumoCam on twitch!",
+    "Touhou 6: Embodiment of a Funky Tree",
+    "Funky 2: Electic Boogalo",
+    "Touhou 7: Perfect Funky blossom",
+    "Touhou 15: Legacy of Funky Kingdom",
+    "Touhou 19: Eternal Funk"
+}
+test = false
+skip = false
+if test == true then msgupdate = "Succesfully compiled"
+elseif msgcheck == nil then msgupdate = "*Quotes loaded succesfully. Version of quotes: \"" .. msgversion .. "\""
+elseif msgcheck == msgversion then skip = true
+else msgupdate = "*Quotes updated succesfully. Version of quotes: \"" .. msgversion .. "\"" end
+if skip == false then game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msgupdate, "All") end
